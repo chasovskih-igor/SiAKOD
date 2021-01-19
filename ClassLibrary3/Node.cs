361b58bc3579374fa528dd76091ee5aece0a74b0
thou-kasatik <71056044+thou-kasatik@users.noreply.github.com>
2021-01-20 00:49:39 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary3/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    class Node<T>
+    {
+        public char Symbol { get; set; }
+        public T Data { get; set; }
+        public bool IsWord { get; set; }
+        public string Prefix { get; set; }
+        public Dictionary<char, Node<T>> SubNodes { get; set; }
+
+
+        public Node(char symbol, T data, string prefix)
+        {
+            Prefix = prefix;
+            Symbol = symbol;
+            Data = data;
+            SubNodes = new Dictionary<char, Node<T>>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Data} [{SubNodes.Count}] ({Prefix})";
+        }
+        public Node<T> TryFind(char symbol)
+        {
+            if (SubNodes.TryGetValue(symbol, out Node<T> value))
+            {
+                return value;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.Equals(item);
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ SiAKOD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary3;
+
+
+
+namespace SiAKOD
+
+{
+    class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+
+        static void Main(string[] args)
+        {
+
+            var trie = new Trie<int>();
+            trie.Add("Привет", 50);
+        }
+        /*{
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }*/
+    }
+}
